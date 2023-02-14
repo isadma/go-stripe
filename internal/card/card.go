1b77daf9afc5d93bd10d3a53cbca201c4b3e9545
Maksat <m.meredov@iqfulfillment.com>
2023-02-14 16:42:43 +0400

    implementation of stripe integration

---
+++ internal/card/card.go
@@ -0,0 +1,55 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusId int
+	Amount              int
+	Currency            string
+	LasFour             string
+	BankReturnCode      string
+}
+
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	//create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	//params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return pi, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined."
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired."
+	default:
+		msg = "Your card was declined."
+	}
+	return msg
+}
