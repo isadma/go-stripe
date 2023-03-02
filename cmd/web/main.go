a3cc48b21c511453792f67af469912852ffd1d7d
Maksat <meredowm645@gmail.com>
2023-03-02 11:55:16 +0400

    db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/isadma/go-stripe/internal/driver"
 )
 
 const version = "1.0.0"
@@ -53,6 +55,7 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environmet {development|production}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "root:@tcp(localhost:3306)/stripe?parseTime=true&tls=false", "DSN")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
 
 	flag.Parse()
@@ -62,6 +65,12 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
 
 	tc := make(map[string]*template.Template)
 
@@ -73,7 +82,7 @@ func main() {
 		version:       version,
 	}
 
-	err := app.serve()
+	err = app.serve()
 
 	if err != nil {
 		app.errorLog.Println(err)
